3089da6248dd20d5556e223b028f1bd372369a12
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2022-06-12 10:37:13 +0100

    Added CI

---
+++ tests/PlaceholderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function test_nothing()
+    {
+        $this->assertTrue(true);
+    }
+}
